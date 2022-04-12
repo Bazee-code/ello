a6a0e79ac271208590eb83c4bd48327293e46b2b
Bazee-code <eugeneobazee2@gmail.com>
2022-04-12 19:05:52 +0300

    Create handleClick event handler

--- src/components/BookPages.js
+++ src/components/BookPages.js
@@ -1,28 +1,31 @@
 import { useQuery } from '@apollo/client';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { GET_BOOK_PAGES } from '../queries/queries';
+import { useNavigate, Link } from 'react-router-dom';
 
 const BookPages = () => {
   const { data, loading, error } = useQuery(GET_BOOK_PAGES);
-  const [text, setText] = useState('');
-  console.log('selected', text);
+  const [selectedText, setSelectedText] = useState('');
+  const navigate = useNavigate();
 
-  useEffect(() => {
-    setText();
-  }, [text]);
+  console.log('selected text', selectedText);
+
+  useState(() => {
+    setSelectedText(selectedText);
+  }, [selectedText]);
 
   if (loading) return <p>Loading pages...</p>;
   if (error) return <p>Error : {error}</p>;
 
-  //   const handleClick = (pageContent, pageToken) => {
-  //     // const subStrings = pageContent.split(' ');
-  //     // const eachString = subStrings.map((string) =>
-  //     //   console.log('string', string)
-  //     // );
-  //     // if (getSelection) {
-  //     //   console.log(getSelection().toString());
-  //     // }
-  //   };
+  const handleClick = (pageToken) => {
+    if (getSelection) {
+      setSelectedText(getSelection().toString());
+    }
+
+    pageToken.map(
+      (token) => token.value === selectedText && navigate(`/${token.value}`)
+    );
+  };
 
   return (
     <div>
@@ -35,8 +38,14 @@ const BookPages = () => {
           return (
             <p
               key={index}
-              style={{ fontSize: 15 }}
-              onClick={() => getSelection && setText(getSelection().toString())}
+              style={{
+                fontSize: 15,
+                cursor: 'pointer',
+              }}
+              onClick={(e) => {
+                // e.persist();
+                handleClick(pageToken);
+              }}
             >
               {pageContent}
             </p>
