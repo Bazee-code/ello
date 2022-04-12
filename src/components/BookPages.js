73f14144fe1844dcadf308f9321eaa63e8a23d26
Bazee-code <eugeneobazee2@gmail.com>
2022-04-12 11:51:30 +0300

    Get user selected text

--- src/components/BookPages.js
+++ src/components/BookPages.js
@@ -1,26 +1,47 @@
 import { useQuery } from '@apollo/client';
-import React from 'react';
+import React, { useState } from 'react';
 import { GET_BOOK_PAGES } from '../queries/queries';
 
 const BookPages = () => {
   const { data, loading, error } = useQuery(GET_BOOK_PAGES);
-
-  console.log('data', data);
+  const [text, setText] = useState('');
+  console.log('selected', text);
 
   if (loading) return <p>Loading pages...</p>;
   if (error) return <p>Error : {error}</p>;
+
+  const handleClick = (pageContent, pageToken) => {
+    // const subStrings = pageContent.split(' ');
+    // const eachString = subStrings.map((string) =>
+    //   console.log('string', string)
+    // );
+    if (getSelection) {
+      console.log(getSelection().toString());
+    }
+  };
+
   return (
     <div>
       <h3>Page Content</h3>
-      <ul>
-        {data && data.book && data.book.pages?.length > 0 ? (
-          data.book.pages.map((page, index) => (
-            <li key={index}>{page.content}</li>
-          ))
-        ) : (
-          <p>Page empty</p>
-        )}
-      </ul>
+
+      {data && data.book && data.book.pages?.length > 0 ? (
+        data.book.pages.map((page, index) => {
+          let pageToken = page.tokens;
+          let pageContent = page.content;
+          return (
+            <p
+              key={index}
+              style={{ fontSize: 15 }}
+              onClick={handleClick}
+              //   onClick={() => getSelection && setText(getSelection().toString())}
+            >
+              {pageContent}
+            </p>
+          );
+        })
+      ) : (
+        <p>Page empty</p>
+      )}
     </div>
   );
 };
