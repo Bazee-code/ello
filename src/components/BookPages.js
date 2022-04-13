ea29a9dd8a98e0b3ab78af70b11d16bce372e174
Bazee-code <eugeneobazee2@gmail.com>
2022-04-13 14:47:55 +0300

    Add some css styles

--- src/components/BookPages.js
+++ src/components/BookPages.js
@@ -3,53 +3,40 @@ import { useNavigate } from 'react-router-dom';
 import Pagination from './Pagination';
 
 const BookPages = ({ data, loading, error, handleClick }) => {
-  // const [selectedText, setSelectedText] = useState('');
-  // const navigate = useNavigate();
-
-  // useEffect(() => {
-  //   setSelectedText(selectedText);
-  // }, [selectedText]);
-
   if (loading) return <p>Loading pages...</p>;
   if (error) return <p>Error : {error}</p>;
 
-  // const handleClick = (pageToken) => {
-  //   if (getSelection) {
-  //     setSelectedText(getSelection().toString());
-  //   }
-
-  //   pageToken.map(
-  //     (token) => token.value === selectedText && navigate(`/${token.value}`)
-  //   );
-  // };
-
   return (
-    <div style={{ paddingLeft: 20 }}>
-      <h3>Page Content</h3>
+    <div style={{ display: 'flex' }}>
+      <div style={{ paddingLeft: 20, width: '70%' }}>
+        <h3>Page Content</h3>
 
-      {data && data.book && data.book.pages?.length > 0 ? (
-        data.book.pages.map((page, index) => {
-          let pageToken = page.tokens;
-          let pageContent = page.content;
-          return (
-            <p
-              key={index}
-              style={{
-                fontSize: 15,
-                cursor: 'pointer',
-              }}
-              onClick={() => {
-                handleClick(pageToken);
-              }}
-            >
-              {pageContent}
-            </p>
-          );
-        })
-      ) : (
-        <p>No books</p>
-      )}
-      <Pagination />
+        {data && data.book && data.book.pages?.length > 0 ? (
+          data.book.pages.map((page, index) => {
+            let pageToken = page.tokens;
+            let pageContent = page.content;
+            return (
+              <p
+                key={index}
+                style={{
+                  fontSize: 15,
+                  cursor: 'pointer',
+                }}
+                onClick={() => {
+                  handleClick(pageToken);
+                }}
+              >
+                {pageContent}
+              </p>
+            );
+          })
+        ) : (
+          <p>No books</p>
+        )}
+      </div>
+      <div style={{ width: '30%', margin: 100 }}>
+        <Pagination />
+      </div>
     </div>
   );
 };
